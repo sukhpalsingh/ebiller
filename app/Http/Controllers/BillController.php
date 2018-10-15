550cd0f948bf774bbc020096ef4593c4a8dc52b7
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2018-10-15 21:39:07 +1000

    Adding auth layer to all controllers

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -10,6 +10,16 @@ use Illuminate\Http\Request;
 
 class BillController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     private $statusOptions = [
         'Pending',
         'Paid'

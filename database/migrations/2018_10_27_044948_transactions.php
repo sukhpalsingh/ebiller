295853f4db20bbff82f19ea99eaeb4973e301147
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2018-10-27 15:00:33 +1000

    Adding transactions table

---
+++ database/migrations/2018_10_27_044948_transactions.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Transactions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('account_id');
+            $table->string('type', 6);
+            $table->string('type_details')->nullable();
+            $table->double('amount', 8, 2);
+            $table->string('description')->nullable();
+            $table->double('balance', 8, 2);
+            $table->timestamps();
+
+            $table->foreign('account_id')->references('id')->on('accounts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}

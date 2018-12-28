192021f3acf757fa4bc8fdf488617ed76dc090f7
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2018-12-28 20:40:52 +1000

    Adding imageupload and fontawesome libs

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,13 +18,17 @@ mix
         'node_modules/bootstrap/dist/css/bootstrap.min.css',
         'node_modules/jquery-ui/themes/base/theme.css',
         'node_modules/jquery-ui/themes/base/datepicker.css',
+        'node_modules/@fortawesome/fontawesome-free/css/all.css',
+        'node_modules/bootstrap-imageupload/dist/css/bootstrap-imageupload.min.css',
         'resources/assets/css/main.css',
    ], 'public/css/lib.css')
    .scripts([
         'node_modules/jquery/dist/jquery.min.js',
-        'node_modules/popper.js/dist/popper.min.js',
+        // 'node_modules/popper.js/dist/popper.min.js',
         'node_modules/bootstrap/dist/js/bootstrap.min.js',
         'node_modules/jquery-ui/ui/widgets/datepicker.js',
+        'node_modules/bootstrap-imageupload/dist/js/bootstrap-imageupload.min.js',
    ], 'public/js/lib.js')
    .copyDirectory('node_modules/jquery-ui/themes/base/images', 'public/css/images')
+   .copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/webfonts')
    ;

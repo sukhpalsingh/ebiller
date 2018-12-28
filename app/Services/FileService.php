2d0fc8f651a893a1d73a6337224005dd802d77e0
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2018-12-28 11:50:07 +1000

    Adding category param when creating file using file service

--- app/Services/FileService.php
+++ app/Services/FileService.php
@@ -11,14 +11,15 @@ class FileService {
     /**
      * Save file record
      * @param Request $request
+     * @param String  $category
      * @return mixed
      */
-    public function save(Request $request)
+    public function save(Request $request, $category = 'bill')
     {
         if ($request->hasFile('file')) {
             $path = Storage::put('files', $request->file('file'), 'private');
             $file = new File();
-            $file->category = 'bill';
+            $file->category = $category;
             $file->path = $path;
             $file->save();
             return $file->id;

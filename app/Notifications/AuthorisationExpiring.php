11454d6280ad2d1cff60ba281506693432b790f3
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2020-04-14 21:11:47 +1000

    removing queueable from autororisation expiring notification

--- app/Notifications/AuthorisationExpiring.php
+++ app/Notifications/AuthorisationExpiring.php
@@ -3,15 +3,10 @@
 namespace App\Notifications;
 
 use App\Http\Controllers\AuthorisationController;
-use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Notification;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class AuthorisationExpiring extends Notification
+class AuthorisationExpiring
 {
-    use Queueable;
-
     private $expiringAuthorisations;
 
     /**

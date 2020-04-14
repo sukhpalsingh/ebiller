cfb0024ffab00708897b8bbb35a01a8faf29624e
Sukhpal Singh <sukhpalsingh@users.noreply.github.com>
2020-04-14 21:20:41 +1000

    Fixing the removal of extend by mistake

--- app/Notifications/AuthorisationExpiring.php
+++ app/Notifications/AuthorisationExpiring.php
@@ -3,9 +3,10 @@
 namespace App\Notifications;
 
 use App\Http\Controllers\AuthorisationController;
+use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class AuthorisationExpiring
+class AuthorisationExpiring extends Notification
 {
     private $expiringAuthorisations;
 
